294a9938fcda493a728e2c5ac455e724c1f98069
Sven Günther <sven.guenther@it-agile.de>
2012-06-01 22:00:44 +0200

    Initial Commit

---
+++ QuirkleTests/QuirkleTests.m
@@ -0,0 +1,32 @@
+//
+//  QuirkleTests.m
+//  QuirkleTests
+//
+//  Created by Sven Günther on 01.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "QuirkleTests.h"
+
+@implementation QuirkleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in QuirkleTests");
+}
+
+@end
