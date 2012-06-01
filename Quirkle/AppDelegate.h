ade3bda32762087d2378cd522f6b368bc7d488b4
Sven Günther <sven.guenther@it-agile.de>
2012-06-01 22:17:35 +0200

    added minimal nessesary Files to start the app

--- Quirkle/AppDelegate.h
+++ Quirkle/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  Quirkle
-//
-//  Created by Sven Günther on 01.06.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
